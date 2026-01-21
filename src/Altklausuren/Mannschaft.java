96d3f05fe884448fba96938038edc3e06f71068e
eviinmotion <eveline.wojackfive@gmail.com>
2026-01-21 15:53:44 +0100

    Altklausuren Aufgaben

--- src/Altklausuren/Mannschaft.java
+++ src/Altklausuren/Mannschaft.java
@@ -2,22 +2,22 @@ package Altklausuren;
 
 public class Mannschaft {
     private String name;
-    private int geschosseneTore;
+    private int tore;
     private int gegentore;
 
     public Mannschaft(String name){
         this.name = name;
-        this.geschosseneTore = 0;
+        this.tore = 0;
         this.gegentore = 0;
     }
     public void setGeschosseneTore(int tore){
-        this.geschosseneTore = tore;
+        this.tore = tore;
     }
     public void setGegentore(int tore){
         this.gegentore = tore;
     }
     public int getGeschosseneTore(){
-        return this.geschosseneTore;
+        return this.tore;
     }
     public int getGegentore(){
         return this.gegentore;
